3c7ac2b7c903fe55cfc884bb9fedc1f6729c65ff
MURILLO COSTA LIMA <murillo.clima@senacsp.edu.br>
2026-02-09 08:56:41 -0300

    Novas Atvidades com PHP e HTML junto

---
+++ Atividade-de-ses.php
@@ -0,0 +1,52 @@
+<?php
+ 
+$estadosBrasil = [
+    'AC' => 'Acre',
+    'AL' => 'Alagoas',
+    'AP' => 'Amapá',
+    'AM' => 'Amazonas',
+    'BA' => 'Bahia',
+    'CE' => 'Ceará',
+    'DF' => 'Distrito Federal',
+    'ES' => 'Espírito Santo',
+    'GO' => 'Goiás',
+    'MA' => 'Maranhão',
+    'MT' => 'Mato Grosso',
+    'MS' => 'Mato Grosso do Sul',
+    'MG' => 'Minas Gerais',
+    'PA' => 'Pará',
+    'PB' => 'Paraíba',
+    'PR' => 'Paraná',
+    'PE' => 'Pernambuco',
+    'PI' => 'Piauí',
+    'RJ' => 'Rio de Janeiro',
+    'RN' => 'Rio Grande do Norte',
+    'RS' => 'Rio Grande do Sul',
+    'RO' => 'Rondônia',
+    'RR' => 'Roraima',
+    'SC' => 'Santa Catarina',
+    'SP' => 'São Paulo',
+    'SE' => 'Sergipe',
+    'TO' => 'Tocantins'
+];
+ ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 2</title>
+</head>
+    <label for="estados"> Selecione um Estado: </label>
+    <select id="estados" name="estado">
+        <?php foreach ($estadosBrasil as $SiglaEstado => $nomeEstado): ?>
+            <option <?= $SiglaEstado ?>> <?= $nomeEstado ?> </option>
+        <?php endforeach; ?>
+    </select>
+    <button> Enviar </button>
+<body>
+ 
+</body>
+ 
+</html>
---
+++ Atividade-de-ses2.php
@@ -0,0 +1,47 @@
+<?php
+ 
+$alunos = [
+    ["nome" => "Ana", "idade" => 18, "nota" => 8.5],
+    ["nome" => "Bruno", "idade" => 20, "nota" => 7.0],
+    ["nome" => "Carlos", "idade" => 19, "nota" => 9.2],
+    ["nome" => "Daniela", "idade" => 21, "nota" => 6.8],
+    ["nome" => "Eduardo", "idade" => 22, "nota" => 5.9],
+    ["nome" => "Fernanda", "idade" => 18, "nota" => 7.8],
+    ["nome" => "Gabriel", "idade" => 20, "nota" => 8.1],
+    ["nome" => "Helena", "idade" => 19, "nota" => 9.0],
+    ["nome" => "Igor", "idade" => 23, "nota" => 6.4],
+    ["nome" => "Juliana", "idade" => 21, "nota" => 7.5],
+    ["nome" => "Lucas", "idade" => 20, "nota" => 8.9],
+    ["nome" => "Mariana", "idade" => 18, "nota" => 9.4],
+];
+ ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 3</title>
+</head>
+<body>
+    <table border="1">
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Nota</th>
+        </tr>
+    </thead>
+    <tbody>
+       <?php foreach ($alunos as $aluno) : ?>
+                <tr>
+                    <td> <?= $aluno["nome"] ?> </td>
+                    <td> <?= $aluno["idade"] ?> </td>
+                    <td> <?= $aluno["nota"] ?> </td>
+                </tr>
+        <?php endforeach; ?>
+        </table>
+    </tbody>
+</body>
+ 
+</html>

01aedff0d3ac794bd32b41a598a97b54cb34ee75
Murillo Costa <murillocosta646@gmail.com>
2026-03-30 08:48:37 -0300

    Update Atividade-1.php
    alterado direito

--- Atividade-do-krl.php
+++ Atividade-do-krl.php
@@ -1,60 +1,40 @@
 <?php
-    $carrinho =
-    [ 
-        ["id" => 1, "nome" => "SSD 512GB", "preco" => 280.00, "quantidade" => 1], 
-        ["id" => 2, "nome" => "Memória RAM 8GB", "preco" => 150.00, "quantidade" => 2], 
-        ["id" => 3, "nome" => "Cabo HDMI 2.0", "preco" => 25.00, "quantidade" => 4], 
-        ["id" => 4, "nome" => "Mouse Gamer", "preco" => 120.00, "quantidade" => 1], 
-        ["id" => 5, "nome" => "Teclado Mecânico", "preco" => 350.00, "quantidade" => 1], 
-        ["id" => 6, "nome" => "Fonte 600W", "preco" => 420.00, "quantidade" => 1], 
-        ["id" => 7, "nome" => "HD Externo 1TB", "preco" => 390.00, "quantidade" => 1], 
-        ["id" => 8, "nome" => "Headset USB", "preco" => 180.00, "quantidade" => 2], 
-    ];
-    
-    $subtotalRAM = $carrinho[1]["preco"] * $carrinho[1]["quantidade"];
-    $subtotalHDMI = $carrinho[2]["preco"] * $carrinho[2]["quantidade"];
-    $subtotalUSB = $carrinho[7]["preco"] * $carrinho[7]["quantidade"];
-    $percentual = 0.10;
-    $percentualAplicado = 0.90;
-    $subtotal = $carrinho[0]["preco"] + $subtotalRAM + $subtotalHDMI + $carrinho[3]["preco"] + $carrinho[4]["preco"] + $carrinho[5]["preco"] + $carrinho[6]["preco"] + $subtotalUSB;
-    $subtotalFinal = $subtotal * (1 - $percentual);
-    $subtotalPercetual = $subtotal * (1 - $percentualAplicado);
+$carrinho = [ 
+    ["id" => 1, "nome" => "SSD 512GB", "preco" => 280.00, "quantidade" => 1], 
+    ["id" => 2, "nome" => "Memória RAM 8GB", "preco" => 150.00, "quantidade" => 2], 
+    ["id" => 3, "nome" => "Cabo HDMI 2.0", "preco" => 25.00, "quantidade" => 4], 
+    ["id" => 4, "nome" => "Mouse Gamer", "preco" => 120.00, "quantidade" => 1], 
+    ["id" => 5, "nome" => "Teclado Mecânico", "preco" => 350.00, "quantidade" => 1], 
+    ["id" => 6, "nome" => "Fonte 600W", "preco" => 420.00, "quantidade" => 1], 
+    ["id" => 7, "nome" => "HD Externo 1TB", "preco" => 390.00, "quantidade" => 1], 
+    ["id" => 8, "nome" => "Headset USB", "preco" => 180.00, "quantidade" => 2], 
+];
 
-    echo $carrinho[0]["nome"]."<br>";
-    echo "Quantidade de Itens = ", $carrinho[0]["quantidade"]."<br>";
-    echo "Subtotal: ", $carrinho[0]["preco"]."<br><br>";
+$subtotal = 0;
 
-    echo $carrinho[1]["nome"]."<br>";
-    echo "Quantidade: ", $carrinho[1]["quantidade"]."<br>";
-    echo "Subtotal: ", $subtotalRAM."<br><br>";
+// acessa o carrinho
+foreach ($carrinho as $item) {
+    $subtotalItem = $item["preco"] * $item["quantidade"];
+    $subtotal += $subtotalItem;
 
-    echo $carrinho[2]["nome"]."<br>";
-    echo "Quantidade: ", $carrinho[2]["quantidade"]."<br>";
-    echo "Subtotal: ", $subtotalHDMI."<br><br>";
+    echo $item["nome"] . "<br>";
+    echo "Quantidade: " . $item["quantidade"] . "<br>";
+    echo "Subtotal: R$ " . number_format($subtotalItem, 2, ',', '.') . "<br><br>";
+}
 
-    echo $carrinho[3]["nome"]."<br>";
-    echo "Quantidade: ", $carrinho[3]["quantidade"]."<br>";
-    echo "Subtotal: ", $carrinho[3]["preco"]."<br><br>";
+echo "<hr>";
 
-    echo $carrinho[4]["nome"]."<br>";
-    echo "Quantidade: ", $carrinho[4]["quantidade"]."<br>";
-    echo "Subtotal: ", $carrinho[4]["preco"]."<br><br>";
+// Regra de desconto
+$desconto = 0;
 
-    echo $carrinho[5]["nome"]."<br>";
-    echo "Quantidade: ", $carrinho[5]["quantidade"]."<br>";
-    echo "Subtotal: ", $carrinho[5]["preco"]."<br><br>";
+if ($subtotal > 1000) {
+    $desconto = $subtotal * 0.10;
+}
 
-    echo $carrinho[6]["nome"]."<br>";
-    echo "Quantidade: ", $carrinho[6]["quantidade"]."<br>";
-    echo "Subtotal: ", $carrinho[6]["preco"]."<br><br>";
+$totalFinal = $subtotal - $desconto;
 
-    echo $carrinho[7]["nome"]."<br>";
-    echo "Quantidade: ", $carrinho[7]["quantidade"]."<br>";
-    echo "Subtotal: ", $subtotalUSB."<br>";
-    
-    echo "<hr>";
-
-    echo "Subtotal da Compra: R$ ".number_format($subtotal, 2, ',', '.')."<br>";
-    echo "Desconto Aplicado (10%): R$ ".number_format($subtotalPercetual, 2, ',', '.')."<br>";
-    echo "Total Final a Pagar: R$ ".number_format($subtotalFinal, 2, ',', '.');
+// Exibição final
+echo "Subtotal da Compra: R$ " . number_format($subtotal, 2, ',', '.') . "<br>";
+echo "Desconto Aplicado: R$ " . number_format($desconto, 2, ',', '.') . "<br>";
+echo "Total Final a Pagar: R$ " . number_format($totalFinal, 2, ',', '.');
 ?>
